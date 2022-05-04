78792e9c2d3564522c12a2e04031635f27d21442
devpoooh <huhu708@naver.com>
2022-05-05 00:20:28 +0900

    2022.05.04

---
+++ src/main/java/swea/Solution.java
@@ -0,0 +1,14 @@
+package swea;
+
+class Solution {
+
+    public static void main(String[] args){
+        int a, b;
+        a = 4 * (1 / 2);
+        System.out.println("a = " + a);
+        b = a++;
+        System.out.println("a = " + a);
+
+        System.out.println("b = " + b);
+    }
+}
